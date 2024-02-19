3021c4a3c108ff489fa55f3fb816d951c8df6a13
LuiV228 <luqmaan.toffey@gmail.com>
2024-02-19 12:28:10 +0200

    updates

--- config/index.js
+++ config/index.js
@@ -2,13 +2,11 @@ import { createPool } from "mysql";
 import { config } from "dotenv";
 config();
 let connection = createPool({
-    host: process.env.HOST,
-    database: process.env.DBName,
-    user: process.env.UserName,
-    password: process.env.UserPass,
-    multipleStatements: true,
-    connectionLimit:30
-})
-export {
-    connection
-}
+  DB_HOST: process.env.DB_HOST,
+  database: process.env.DB_Name,
+  user: process.env.DB_UserName,
+  password: process.env.DB_UserPass,
+  multipleStatements: true,
+  connectionLimit: 30,
+});
+export { connection };
